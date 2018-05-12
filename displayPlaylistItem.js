cacee94c2a13f09f8fef463d96588c8c0d50a7fd
alincode <alincode@gmail.com>
2018-05-12 13:20:37 +0800

    adjust output format

--- displayPlaylistItem.js
+++ displayPlaylistItem.js
@@ -4,6 +4,7 @@ const displayPlaylistItem = async function (config, playlistId) {
   try {
     const ps = new PlaylistSummary(config);
     let playlist = await ps.getPlaylistItems(playlistId);
+    console.log(`### ${playlist.channelTitle} : ${playlist.playlistTitle}\n`);
     playlist.items.forEach(function (item, index) {
       console.log(`* ${item.title}`)
     });
